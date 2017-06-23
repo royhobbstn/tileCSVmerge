d875bb654226cb894b13cff843a5d9ae868353d4
Daniel Trone <royhobbstn@yahoo.com>
2017-06-23 11:32:41 +0000

    initial commit

---
+++ merge_columns.js
@@ -0,0 +1,87 @@
+var csv = require('csv');
+var fs = require('fs');
+
+var file1 = new Promise((resolve, reject) => {
+    fs.readFile('./readyfiles/heseq001.csv', 'utf8', function(err, data) {
+        if (err) {
+            reject(err);
+        }
+
+        csv.parse(data, {}, function(err, output) {
+            if (err) {
+                reject(err);
+            }
+
+            resolve(output);
+        });
+
+    });
+});
+
+var file2 = new Promise((resolve, reject) => {
+    fs.readFile('./readyfiles/heseq002.csv', 'utf8', function(err, data) {
+        if (err) {
+            reject(err);
+        }
+
+        csv.parse(data, {}, function(err, output) {
+            if (err) {
+                reject(err);
+            }
+
+            var sliced = output.map(function(d) {
+                return d.slice(54);
+            });
+
+            resolve(sliced);
+        });
+
+    });
+});
+
+var file3 = new Promise((resolve, reject) => {
+    fs.readFile('./readyfiles/heseq003.csv', 'utf8', function(err, data) {
+        if (err) {
+            reject(err);
+        }
+
+        csv.parse(data, {}, function(err, output) {
+            if (err) {
+                reject(err);
+            }
+
+
+            var sliced = output.map(function(d) {
+                return d.slice(54);
+            });
+
+            resolve(sliced);
+        });
+
+    });
+});
+
+
+Promise.all([file1, file2, file3]).then(function(data) {
+
+    const combined = data[0].map((d, i) => {
+        return d.concat(data[1][i]).concat(data[2][i]);
+    });
+
+    console.log(combined[0]);
+
+    csv.stringify(combined, function(err, data) {
+        if (err) {
+            return console.log(err);
+        }
+
+        fs.writeFile("./readyfiles/test.csv", data, function(err) {
+            if (err) {
+                return console.log(err);
+            }
+
+            console.log("The file was saved!");
+        });
+    });
+
+});

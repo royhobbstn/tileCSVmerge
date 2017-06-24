7cee5de4c9d9a181b371aa8172f25874119c5a1f
Daniel Trone <royhobbstn@users.noreply.github.com>
2017-06-24 13:36:26 -0500

    Update merge_columns.js pipe to write stream

--- merge_columns.js
+++ merge_columns.js
@@ -1,9 +1,11 @@
 var csv = require('csv');
 var fs = require('fs');
 
+var stream = fs.createWriteStream("my_file.txt");
 fs.createReadStream(__dirname+'/run/readyfiles/heseq001.csv').pipe(
 csv.parse     ()).pipe(
 csv.transform (function(record){
                 return record.slice(54,-1);
               })).pipe(
-csv.stringify ()).pipe(process.stdout);
+csv.stringify ()).pipe(stream);
+

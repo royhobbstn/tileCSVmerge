c791b3056be9cd1ba167cd0305b445284e442e8c
Daniel Trone <royhobbstn@yahoo.com>
2017-06-25 12:08:37 +0000

    settle for hacky workaround

--- create_multisequence_files.sh
+++ create_multisequence_files.sh
@@ -1,52 +1,64 @@
 
+echo "install nodeJS";
 wget -qO- https://raw.githubusercontent.com/creationix/nvm/v0.33.2/install.sh | bash
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
 nvm install node
 
-
+echo "clean any previous installations";
 rm -r node_modules
 rm -r run
 rm -r result
 
+echo "install JS dependencies";
 npm install
 
 mkdir run
 mkdir result
+mkdir result_orig
 
 cd run
 
-mkdir schemas; mkdir readyfiles
+mkdir schemas
+mkdir readyfiles
 
+echo "downloading census metadata";
 curl --progress-bar https://www2.census.gov/programs-surveys/acs/summary_file/2015/documentation/user_tools/ACS_5yr_Seq_Table_Number_Lookup.txt -O
 
+echo "strip header row from metadata";
 sed 1d ACS_5yr_Seq_Table_Number_Lookup.txt > no_header.csv
 
 awk -F, '$4 ~ /^[0-9]+$/' no_header.csv > columns_list.csv
 
+echo "populating schema files with basic column information"
 n=122;for i in $(seq -f "%04g" ${n});do echo -n "KEY,FILEID,STUSAB,SUMLEVEL,COMPONENT,LOGRECNO,US,REGION,DIVISION,STATECE,STATE,COUNTY,COUSUB,PLACE,TRACT,BLKGRP,CONCIT,AIANHH,AIANHHFP,AIHHTLI,AITSCE,AITS,ANRC,CBSA,CSA,METDIV,MACC,MEMI,NECTA,CNECTA,NECTADIV,UA,BLANK1,CDCURR,SLDU,SLDL,BLANK2,BLANK3,ZCTA5,SUBMCD,SDELM,SDSEC,SDUNI,UR,PCI,BLANK4,BLANK5,PUMA5,BLANK6,GEOID,NAME,BTTR,BTBG,BLANK7" > "./schemas/schema$i.txt"; done;
+
+echo "appending additional columns to each table according to metadata"
 while IFS=',' read f1 f2 f3 f4 f5; do echo -n ","`printf $f2`"_"`printf %03d $f4`"" >> "./schemas/schema$f3.txt"; done < columns_list.csv;
 
-wget https://storage.googleapis.com/acs1115_stage/eseq001.csv; wget https://storage.googleapis.com/acs1115_stage/eseq002.csv; wget https://storage.googleapis.com/acs1115_stage/eseq003.csv;
-
+echo "downloading all census estimate sequence files in bucket"
 # gsutil cp gs://acs1115_stage/e*.csv
 
+wget https://storage.googleapis.com/acs1115_stage/eseq001.csv; 
 
+wget https://storage.googleapis.com/acs1115_stage/eseq002.csv; 
+
+wget https://storage.googleapis.com/acs1115_stage/eseq003.csv;
+
+echo "concatenating schema and data files for each sequence"
 n=122;for i in $(seq -f "%03g" ${n}); do echo -n -e ",\n" >> ./schemas/schema0$i.txt; cat ./schemas/schema0$i.txt eseq$i.csv > ./readyfiles/eseq$i.csv; done;
-
-
-head -1000 ./readyfiles/eseq001.csv > ./readyfiles/heseq001.csv
-tail -1000 ./readyfiles/eseq001.csv >> ./readyfiles/heseq001.csv
-head -1000 ./readyfiles/eseq002.csv > ./readyfiles/heseq002.csv
-tail -1000 ./readyfiles/eseq002.csv >> ./readyfiles/heseq002.csv
-head -1000 ./readyfiles/eseq003.csv > ./readyfiles/heseq003.csv
-tail -1000 ./readyfiles/eseq003.csv >> ./readyfiles/heseq003.csv
 
 cd ..
 
+echo "creating google storage bucket for stripped sequence files"
 gsutil mb gs://acs1115_multisequence
 
+echo "running nodejs program to strip first 55 columns of redundant information"
 node merge_columns.js
 
+paste -d',' ./result_orig/eseq001.csv ./result/eseq002.csv ./result/eseq003.csv > paste.csv
+
+
+echo "loading all processed data into storage bucket"
 gsutil cp ./result/*.csv gs://acs1115_multisequence
--- merge_columns.js
+++ merge_columns.js
@@ -1,11 +1,37 @@
 var csv = require('csv');
 var fs = require('fs');
 
-var stream = fs.createWriteStream("my_file.txt");
-fs.createReadStream(__dirname+'/run/readyfiles/heseq001.csv').pipe(
-csv.parse     ()).pipe(
-csv.transform (function(record){
-                return record.slice(54,-1);
-              })).pipe(
-csv.stringify ()).pipe(stream);
 
+fs.readdir('./run/readyfiles/', (err, files) => {
+    if (err) return err;
+
+    files.forEach(file => {
+
+        var stream = fs.createWriteStream(__dirname + '/result/' + file);
+        var stream_orig = fs.createWriteStream(__dirname + '/result_orig/' + file);
+
+        fs.createReadStream(__dirname + '/run/readyfiles/' + file).pipe(
+            csv.parse()).pipe(
+            csv.transform(function(record) {
+                return record.slice(54, -1);
+            })).pipe(
+            csv.stringify()).pipe(stream);
+
+        fs.createReadStream(__dirname + '/run/readyfiles/' + file).pipe(
+            csv.parse()).pipe(
+            csv.transform(function(record) {
+                return record.slice(54, -1);
+            })).pipe(
+            csv.stringify()).pipe(stream_orig);
+
+        stream.on('finish', function() {
+            console.log(file + ' has been written');
+        });
+
+        stream_orig.on('finish', function() {
+            console.log(file + '_orig has been written');
+        });
+
+    });
+
+});
